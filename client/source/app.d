68a830ab5d3f2f1666f8eb64d95072f467da2d1c
Nikolay Tolstokulakov <ntolstokulakov@setronica.com>
2014-09-03 01:00:25 +0700

    wip

--- client/source/app.d
+++ client/source/app.d
@@ -17,31 +17,37 @@ void main()
 			auto payload = new ubyte[16];//cast(ubyte[])"Hello, World!\r\n";//
 			auto udp_sender =  listenUDP(0xc2d5);
 			//udp_sender.connect("127.0.0.1", 1234);
-			udp_sender.connect("10.0.2.129", 1234);
+			//udp_sender.connect("10.0.2.2", 1234);
+			udp_sender.connect("192.168.56.1", 1234);
 
 
 			StopWatch sw;
 			enum n = 1;
-			enum udp_count = 100;
+			enum udp_count = 1_000;
 			TickDuration[n] times;
 			TickDuration last = TickDuration.from!"seconds"(0);
 			foreach(i; 0..n)
 			{
 				logInfo("attempt: %s", i);
 				sw.start(); //start/resume mesuring.
-				for(int k=0; k<udp_count; k++){
+				for(int k=1; k<udp_count; k++){
 					//sleep(dur!"msecs"(1));
-					yield();
+					//yield();
 					uint* ptr = (cast(uint*)payload.ptr);
 					*ptr = k;
 					udp_sender.send(payload);
+					//logInfo("sent;  %s", k);
 				}
+
 
 				sw.stop();  //stop/pause measuring.
 				//Return value of peek() after having stopped are the always same.
 				logInfo("%s times done, lap time: %s[ms], speed: %s", (i + 1) * udp_count, sw.peek().msecs, (1000.0f*(i + 1) * udp_count/sw.peek().msecs));
 				times[i] = sw.peek() - last;
 				last = sw.peek();
+				logInfo("yield <-");
+				yield();
+				logInfo("yield ->");
 			}
 			real sum = 0;
 			// To know the number of seconds,
@@ -51,7 +57,7 @@ void main()
 				sum += t.msecs;
 			logInfo("Average time: %s msecs", sum/n);
 
-		}catch(Exception e){
+		}catch(Throwable e){
 			logError("%s", e);
 		}
 	});
